880ca8ae14dfc2295f761aceb97a70a29550f9b2
usbbush <jeanbienvenusb@gmail.com>
2023-02-13 02:17:07 +0200

    Write test for sayNumber function

--- sections/C/tests/unit.test.ts
+++ sections/C/tests/unit.test.ts
@@ -4,7 +4,8 @@ import {
   groupInChunksOfThree,
   handleTens,
   handleHundreds,
-  handleSmallNumber, numberToWords, capitalizeFirstLetter
+  handleSmallNumber, numberToWords, capitalizeFirstLetter,
+  sayNumber
 } from '../index';
 
 describe('function ', () => {
@@ -87,21 +88,57 @@ describe('function ', () => {
     });
   });
   describe('numberToWords()', function () {
-    it('should return "one million." when input is 1,000,000', function () {
-      expect(numberToWords(1000000)).toBe('one million.');
+    it('should return "one million" when input is 1,000,000', function () {
+      expect(numberToWords(1000000)).toBe('one million');
     });
-    it('should return "one hundred and twenty-one." when input is 121', () => {
-      expect(numberToWords(121).trim()).toBe('one hundred and twenty-one.');
+    it('should return "one hundred and twenty-one" when input is 121', () => {
+      expect(numberToWords(121).trim()).toBe('one hundred and twenty-one');
     });
-    it('should return "ninety trillion, three hundred and seventy six billion, ten thousand and twelve." when input is 90376000010012', () => {
-      expect(numberToWords(90376000010012).trim()).toBe('ninety trillion, three hundred and seventy-six billion, ten thousand and twelve.');
+    it('should return "ninety trillion, three hundred and seventy six billion, ten thousand and twelve" when input is 90376000010012', () => {
+      expect(numberToWords(90376000010012).trim()).toBe('ninety trillion, three hundred and seventy-six billion, ten thousand and twelve');
     });
   });
 
   describe('capitalizeFirstLetter()', function () {
     it('should return "Hello" when input is hello', () => {
-      expect(capitalizeFirstLetter('hello')).toBe('Hello')
-    })
-  })
+      expect(capitalizeFirstLetter('hello')).toBe('Hello');
+    });
+  });
+
+  describe('sayNumber()', function () {
+    it('should return "Zero."  when input is 0', () => {
+      expect(sayNumber(0)).toBe('Zero.');
+    });
+    it('should return "Eleven."  when input is 11', () => {
+      expect(sayNumber(11)).toBe('Eleven.');
+    });
+    it('should return "Fourteen."  when input is 14', () => {
+      expect(sayNumber(14)).toBe('Fourteen.');
+    });
+    it('should return "Fifteen."  when input is 15', () => {
+      expect(sayNumber(15)).toBe('Fifteen.');
+    });
+    it('should return "Forty-three."  when input is 43', () => {
+      expect(sayNumber(43)).toBe('Forty-three.');
+    });
+    it('should return "Fifty."  when input is 50', () => {
+      expect(sayNumber(50)).toBe('Fifty.');
+    });
+    it('should return "One thousand and one."  when input is 1001', () => {
+      expect(sayNumber(1001)).toBe('One thousand and one.');
+    });
+    it('should return "Twenty thousand."  when input is 20000', () => {
+      expect(sayNumber(20000)).toBe('Twenty thousand.');
+    });
+    it('should return "One million, thirty-three thousand, two hundred and eighty-six."  when input is 1033286', () => {
+      expect(sayNumber(1033286)).toBe('One million, thirty-three thousand, two hundred and eighty-six.');
+    });
+    it('should return "Twelve million and thirteen."  when input is 12000013', () => {
+      expect(sayNumber(12000013)).toBe('Twelve million and thirteen.');
+    });
+    it('should return "Ninety trillion, three hundred and seventy-six billion, ten thousand and twelve."  when input is 90376000010012', () => {
+      expect(sayNumber(90376000010012)).toBe('Ninety trillion, three hundred and seventy-six billion, ten thousand and twelve.');
+    });
+  });
 });
 
